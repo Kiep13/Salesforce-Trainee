c85ac872fdd9a2c4f4309e9a755c3f72efdcbd17
nina_smirnova <kiep13@bk.ru>
2020-11-02 15:35:36 +0300

    added button for generating pdf

---
+++ Task 2/force-app/main/default/lwc/generatePdfButton/generatePdfButton.js
@@ -0,0 +1,44 @@
+import { LightningElement, api, track } from 'lwc';
+import GENERATE_PDF_FIELD from '@salesforce/schema/Account.Generate_PDF__c';
+import ID_FIELD from '@salesforce/schema/Account.Id';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { updateRecord } from 'lightning/uiRecordApi';
+
+export default class GeneratePdfButton extends LightningElement {
+
+  SUCSESS_MESSAGE = 'PDF generated!';
+
+  ERROR_TITLE = 'Error generating PDF!';
+  ERROR_VARIANT = 'error';
+
+  @api recordId;
+  @track processing; 
+  @track resultMessage;
+
+  connectedCallback() {
+    this.resultMessage = 'Processing your request...';
+    this.processing = true;
+    this.generatePdf();
+  }
+
+  generatePdf() {
+    // const closeAction = new CustomEvent('close');
+    // this.dispatchEvent(closeAction);
+
+    const fields = {};
+    fields[ID_FIELD.fieldApiName] = this.recordId;
+    fields[GENERATE_PDF_FIELD.fieldApiName] = true;
+
+    const recordInput = { fields };
+
+    updateRecord(recordInput)
+                .then(() => {
+                  this.resultMessage = this.SUCSESS_MESSAGE;
+                  this.processing = false;
+                })
+                .catch(error => {
+                  this.resultMessage = error.body.message;
+                  this.processing = false;
+                });
+  }
+}
