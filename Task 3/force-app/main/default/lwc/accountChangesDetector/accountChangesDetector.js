478e33f80fac23f2eb9180a8ca49e9569650341d
nina_smirnova <kiep13@bk.ru>
2020-11-14 20:33:52 +0300

    improved logic of event detecting

--- Task 3/force-app/main/default/lwc/accountChangesDetector/accountChangesDetector.js
+++ Task 3/force-app/main/default/lwc/accountChangesDetector/accountChangesDetector.js
@@ -1,19 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 import { subscribe, unsubscribe, onError } from 'lightning/empApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+import MESSAGE_FIELD from '@salesforce/schema/Account_Change__e.Message__c';
+import ID_FIELD from '@salesforce/schema/Account_Change__e.Id__c';
+
 export default class AccountChangesDetector extends LightningElement {
+
+  @api recordId;
+
   channelName = '/event/Account_Change__e';
 
   subscription = {};
 
   connectedCallback() {    
-      const messageCallback = (response) => {
-      const message = response['data']['payload']['Message__c'];
-
-      console.log(message);
-
-      this.showToast('Account changes', message);
+    const messageCallback = (response) => {
+      if(this.recordId == response.data.payload[ID_FIELD.fieldApiName]) {
+        const message = response.data.payload[MESSAGE_FIELD.fieldApiName];
+        this.showToast('Account changes', message);
+      }
     };
 
     subscribe(this.channelName, -1, messageCallback).then(response => {
